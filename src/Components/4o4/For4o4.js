a19708186c00cfcea5fca95e789fab1b20aa27e4
masniloy <masaimunniloy88@gmail.com>
2022-10-30 20:52:42 +0600

    work on 404 problem

--- src/Components/4o4/For4o4.js
+++ src/Components/4o4/For4o4.js
@@ -3,7 +3,9 @@ import React from 'react';
 const For4o4 = () => {
     return (
         <div>
-            <h1>its work</h1>
+            <br /><br /><br /><h1>4o4</h1>
+            <h4>Page not found..!!</h4>
+            <p>Hopefully the site navigation can help you find what you need. </p>
         </div>
     );
 };
